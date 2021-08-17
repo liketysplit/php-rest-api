297a97d26c03df5841efc9d36b22b7f9a2380bd6
Jordan Boles <jordanb@Jordans-MBP.localdomain>
2021-08-16 22:48:54 -0400

    adding a watcher if user and topic id exists, adding reply if the same logic holds, checking for existing watch on the topic as not to duplicate data

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Replies;
+use App\Models\Topics;
+use App\Models\Watchers;
+use App\Models\Users;
 
 class RepliesController extends Controller
 {
@@ -31,7 +34,36 @@ class RepliesController extends Controller
             'topic_id' => 'required|integer'
         ]);
 
-        return Replies::create($request->all());
+        $user = Users::where('id', '=', $request->user_id)->first();
+        $topic = Topics::where('id', '=', $request->topic_id)->first();
+        
+
+        $errors = [];
+        if ($user === null) {
+            array_push($errors, 'No such user id exists.');
+        }
+
+        if ($topic === null) {
+            array_push($errors, 'No such topic id exists.');
+        }
+
+        if (count($errors) > 0) {
+            return $errors;
+        }
+
+        $reply = Replies::create($request->all());
+
+        $watcher = Watchers::where('topic_id', '=', $request->topic_id)->where('user_id', '=', $request->user_id)->first();
+
+        if ($watcher === null) {
+
+            Watchers::create([
+                'topic_id' => $reply->topic_id,
+                'user_id' => $reply->user_id,
+            ]);
+        }
+
+        return $reply;
     }
 
     /**
