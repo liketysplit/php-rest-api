4423c25dc5a96ca5161884275038b852c14ea6ef
Jordan Boles <jordanb@Jordans-MBP.localdomain>
2021-08-16 21:02:28 -0400

    removing timestamps caused issue on create expected craete_at and updated_at

--- app/Models/Watchers.php
+++ app/Models/Watchers.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Watchers extends Model
 {
     use HasFactory;
+    public $timestamps = false;
+    protected $fillable = [
+        'user_id', 
+        'topic_id'
+    ];
 }
